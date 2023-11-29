154eeb993da096297823afa128cd65bcaa3139b5
Kabelo Drew Tshehla <66612725+KayDrew@users.noreply.github.com>
2023-11-29 16:49:26 +0200

    Update expenses.js

--- routes/expenses.js
+++ routes/expenses.js
@@ -1,13 +1,24 @@
 export  default function expenses(query){
-
+let totalAmount=0;
+let weekly=0;
+let monthly=0;
+let daily=0;
+let onceOff=0;
+let weekend=0;
+let weekday=0;
+let expenses=[];
 let message="";
 let regex= /^([a-zA-Z]{3,})$/;
 
 async function home(req,res){
+expenses=[{name:"Monthly", amount:monthly},
+{name:"Daily",amount:daily},{name:"Once-Off",amount:onceOff},
+{name:"Weekly",amount:weekly},{name:"Weekday",amount:weekday},{name:"Weekend",amount:weekend}];
+
 
 req.flash("message",message);
 
-res.render("index",{
+res.render("index",{totalAmount,expenses 
 });
 }
 
@@ -44,9 +55,11 @@ case "Once-Off"  :
 id= 6;
 break;
 
+
+
 }
 
-console.log(id);
+
 
 if(description){
 	
@@ -87,22 +100,88 @@ message="Please, enter  description";
 }
 
 
-res.redirect("/");
+
+
+let allItems= await query.getExpenses();
+var weekly1=0;
+var daily1=0;
+var monthly1=0;
+var weekend1=0;
+var weekday1=0;
+var onceOff1=0;
+let totalAmount1=0;
+
+for(let i=0;i<allItems.length;++i){
+
+var item= allItems[i];
+var name=item.name;
+
+totalAmount1+= item.amount;
+
+switch(item.name){
+
+case "Daily":
+
+daily1+=item.amount;
+
+break;
+
+case "Monthly":
+monthly1+=item.amount;
+
+break;
+
+case "Once-Off":
+onceOff1+=item.amount;
+
+break;
+
+case "Weekly":
+weekly1+=item.amount;
+
+break;
+
+case "Weekend":
+weekend1+=item.amount;
+
+break;
+
+case "Weekday":
+weekday1+=item.amount;
+
+break;
+
 
 
 }
 
-async function getExpenses(req,res){
-
-let expenses= await query.getExpenses();
+}
 
 
+monthly=monthly1;
+daily=daily1*30;
+onceOff=onceOff1;
+weekly=weekly1*4;
+weekday=weekday1*5;
+weekend=weekend1*4;
+
+totalAmount= monthly+daily+onceOff+weekly+weekday+weekend;
+res.redirect("/");
+}
+
+async  function clearExpenses(req,res){
+
+await query.clearExpenses();
+expenses=[];
+totalAmount=0;
+message="deleted";
+res.redirect("/");
 }
 
 
 return{
 home,
 recordExpense,
-getExpenses 
+clearExpenses 
 }
-}
+	}
